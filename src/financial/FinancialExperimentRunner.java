a9b8f481c6fbefd84ed444a8e2817c1de810a148
Felix Weiland <fweiland@mail.upb.de>
2018-11-08 16:22:02 +0100

    New version of the experiment runner

--- src/financial/FinancialExperimentRunner.java
+++ src/financial/FinancialExperimentRunner.java
@@ -7,11 +7,15 @@ import java.util.Map;
 import java.util.Random;
 
 import org.aeonbits.owner.ConfigCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import autofe.db.configuration.DatabaseAutoFeConfiguration;
+import autofe.db.model.database.AbstractFeature;
+import autofe.db.util.DBUtils;
 import autofe.processor.DatabaseProcessor;
-import de.upb.crc901.mlplan.multiclass.DefaultPreorder;
-import de.upb.crc901.mlplan.multiclass.MLPlan;
+import de.upb.crc901.mlplan.multiclass.wekamlplan.MLPlanWekaClassifier;
+import de.upb.crc901.mlplan.multiclass.wekamlplan.weka.WekaMLPlanWekaClassifier;
 import jaicore.basic.SQLAdapter;
 import jaicore.experiments.ExperimentDBEntry;
 import jaicore.experiments.ExperimentRunner;
@@ -21,8 +25,11 @@ import jaicore.experiments.IExperimentSetEvaluator;
 import jaicore.ml.WekaUtil;
 import weka.classifiers.Evaluation;
 import weka.core.Instances;
+import weka.core.converters.ArffSaver;
 
 public class FinancialExperimentRunner {
+
+	private static Logger LOG = LoggerFactory.getLogger("EXPERIMENT");
 
 	public static void main(String[] args) {
 		FinancialConfig m = ConfigCache.getOrCreate(FinancialConfig.class);
@@ -37,42 +44,78 @@ public class FinancialExperimentRunner {
 			@Override
 			public void evaluate(ExperimentDBEntry experimentEntry, SQLAdapter adapter,
 					IExperimentIntermediateResultProcessor processor) throws Exception {
-				// Get experiment setup
-				Map<String, String> description = experimentEntry.getExperiment().getValuesOfKeyFields();
-				int timeoutInMs = Integer.parseInt(description.get("timeout"));
-				long seed = Long.parseLong(description.get("seed"));
-				int randomCompletionPathLength = Integer.parseInt(description.get("randomcompletion_pathlength"));
-				String evaluationFunction = description.get("evaluation_function");
-				String dbModelFile = m.getDatabaseModelFile();
-				DatabaseAutoFeConfiguration config = new DatabaseAutoFeConfiguration(randomCompletionPathLength,
-						evaluationFunction, seed, timeoutInMs);
-				DatabaseProcessor dbProcessor = new DatabaseProcessor(config, dbModelFile);
-
-				// Get features
-				Instances instances = dbProcessor.selectFeatures();
-
-				// Evaluate features using ML-Plan
-				List<Instances> split = WekaUtil.getStratifiedSplit(instances, new Random(0), .7f);
-				MLPlan mlplan = new MLPlan(new File("model/weka/weka-all-autoweka.json"));
-				mlplan.setLoggerName("mlplan");
-				mlplan.setTimeout(timeoutInMs);
-				mlplan.setPortionOfDataForPhase2(.3f);
-				mlplan.setNodeEvaluator(new DefaultPreorder());
-				mlplan.enableVisualization();
-				mlplan.buildClassifier(split.get(0));
-
-				Evaluation eval = new Evaluation(split.get(0));
-				eval.evaluateModel(mlplan, split.get(1));
-				double accuracy = eval.pctCorrect();
-
-				Map<String, Object> results = new HashMap<>();
-				results.put("score", accuracy);
-				processor.processResults(results);
+				try {
+					doExperiment(experimentEntry, adapter, processor, m);
+				} catch (Exception e) {
+					LOG.error("Experiment failed!", e);
+					throw e;
+				}
 			}
 		});
 
 		runner.randomlyConductExperiments(true);
 
+	}
+
+	private static void doExperiment(ExperimentDBEntry experimentEntry, SQLAdapter adapter,
+			IExperimentIntermediateResultProcessor processor, FinancialConfig m) throws Exception {
+		LOG.info("Starting experiment");
+		
+		// Get experiment setup
+		Map<String, String> description = experimentEntry.getExperiment().getValuesOfKeyFields();
+		int timeoutInMs = Integer.parseInt(description.get("timeout"));
+		long seed = Long.parseLong(description.get("seed"));
+		double feFraction = Double.parseDouble(description.get("fe_fraction"));
+		int randomCompletionPathLength = Integer.parseInt(description.get("randomcompletion_pathlength"));
+		String evaluationFunction = description.get("evaluation_function");
+
+		LOG.info("Setup is: timeout={}, seed={}, feFraction={}, rcPathLength={}, evalFunction={}", timeoutInMs, seed,
+				feFraction, randomCompletionPathLength, evaluationFunction);
+
+		String dbModelFile = m.getDatabaseModelFile();
+		DatabaseAutoFeConfiguration config = new DatabaseAutoFeConfiguration(randomCompletionPathLength,
+				evaluationFunction, seed, (int) (timeoutInMs * feFraction));
+		LOG.info("Starting feature extraction..");
+		DatabaseProcessor dbProcessor = new DatabaseProcessor(config, dbModelFile);
+		dbProcessor.doFeatureSelection();
+		LOG.info("Finshed feature extraction..");
+
+		// Get features
+		List<AbstractFeature> selectedFeatures = dbProcessor.getSelectedFeatures();
+		Instances instances = dbProcessor.getInstancesWithSelectedFeatures();
+
+		LOG.info("Selected {} features", selectedFeatures.size());
+
+		// Save instances to ARFF file for debugging purposes
+		ArffSaver s = new ArffSaver();
+		s.setInstances(instances);
+		s.setFile(new File("data.arff"));
+		s.writeBatch();
+
+		LOG.info("Starting ML-Plan");
+
+		// Evaluate features using ML-Plan
+		List<Instances> split = WekaUtil.getStratifiedSplit(instances, new Random(0), .7f);
+		MLPlanWekaClassifier mlplan = new WekaMLPlanWekaClassifier();
+		mlplan.setLoggerName("mlplan");
+		mlplan.setTimeout((int) ((timeoutInMs / 1000) * (1 - feFraction)));
+		mlplan.setTimeoutForNodeEvaluation(15);
+		mlplan.setTimeoutForSingleSolutionEvaluation(15);
+		mlplan.setPortionOfDataForPhase2(.3f);
+		mlplan.buildClassifier(split.get(0));
+		mlplan.setNumCPUs(m.getNumberOfCPUs());
+
+		Evaluation eval = new Evaluation(split.get(0));
+		eval.evaluateModel(mlplan, split.get(1));
+		double accuracy = eval.pctCorrect();
+
+		LOG.info("Finished experiment, score is {}", accuracy);
+
+		Map<String, Object> results = new HashMap<>();
+		results.put("score", accuracy);
+		results.put("no_selected_features", selectedFeatures.size());
+		results.put("selected_features", selectedFeatures.toString());
+		processor.processResults(results);
 	}
 
 }
